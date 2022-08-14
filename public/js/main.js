aeac4aa386284a1cfa41e8a69ef3920c390c7953
Edmond GAKS <edmondgaks@gmail.com>
2022-08-14 12:36:16 +0200

    adding t=many different messages like on disconnect,etc

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
+socket.on('message',message => {
+    console.log(message);
+});
+
+// Message submit 
+
--- server.js
+++ server.js
@@ -12,6 +12,20 @@ app.use(express.static('public'));
 
 io.on('connection', socket => {
     console.log('New Ws connection');
+    socket.emit('message', 'Welcome to the pChat');
+    // to the single client
+    // Broadcast when a user connects
+    
+    socket.broadcast.emit('message', 'A  user has joined the chat');
+    // to all clients except the user who connects
+
+    // io.emit()
+    // to all clients
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    })
 });
 
 const PORT = process.env.PORT || 5000;
