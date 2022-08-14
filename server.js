1936f4a13162b7eb2f0e5fb1f62ce4caab14dea0
Edmond GAKS <edmondgaks@gmail.com>
2022-08-14 12:18:44 +0200

    starting to use socket.io

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 const app = express();
-app.use(express.static('public'));
+const server = http.createServer(app);
 
+const io = socketio(server);
+
+app.use(express.static('public'));
 // or app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+    console.log('New Ws connection');
+})
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => {console.log(`Server running on port ${PORT}`);});
